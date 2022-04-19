bff79d17812525114a4faf6756a136522a745c9c
bob bob <71616101+testUser453@users.noreply.github.com>
2022-04-19 16:14:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    int num1 = 2, num2 = 12, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+  }
+}
